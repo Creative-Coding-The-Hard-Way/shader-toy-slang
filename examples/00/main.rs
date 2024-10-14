6900e80d90c3e8003dcb7504aa36d2687265ffe4
Bradley Lyman <lyman.brad3211@gmail.com>
2024-10-13 20:45:03 -0700

    setup the glfw application structure

--- examples/00/main.rs
+++ examples/00/main.rs
@@ -1,3 +1,37 @@
+use {
+    anyhow::Result,
+    glfw::{Action, Key, WindowEvent},
+    sts::app::{app_main, App},
+};
+
+struct Example {}
+
+impl App for Example {
+    fn new(window: &mut glfw::Window) -> Result<Self>
+    where
+        Self: Sized,
+    {
+        window.set_all_polling(true);
+        log::info!("hello world");
+        Ok(Self {})
+    }
+
+    fn handle_event(
+        &mut self,
+        window: &mut glfw::Window,
+        event: glfw::WindowEvent,
+    ) -> Result<()> {
+        if let WindowEvent::Key(Key::Escape, _, Action::Release, _) = event {
+            window.set_should_close(true);
+        }
+        Ok(())
+    }
+
+    fn update(&mut self, _window: &mut glfw::Window) -> Result<()> {
+        Ok(())
+    }
+}
+
 pub fn main() {
-    sts::aoeu();
+    app_main::<Example>();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
-pub fn aoeu() {
-    println!("Hello World!");
+pub mod app;
+
+/// Add context to an anyhow error which includes a formatted error message, the
+/// file path, and the line number.
+///
+/// # Usage
+///
+/// Given a method which returns an anyhow result:
+///
+/// my_method()
+///     .with_context(!trace("some error message {}", some_variable))?;
+#[macro_export]
+macro_rules! trace {
+    ($($arg:tt)*) => {{
+        || {
+            let res = format!("{}:{} - {}", file!(), line!(), format!($($arg)*));
+            res
+        }
+    }}
 }
