b0d2a510b7a8acdf14f71459b452ab8c241845f8
BradleyLyman <BradleyLyman@users.noreply.github.com>
2024-10-27 23:42:03 +0000

    deploy: 1d8e1cef8e985ca50f5219d435f9467fcbd1864e

--- sts/graphics/vulkan/sidebar-items.js
+++ sts/graphics/vulkan/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AcquireImageStatus","FrameStatus","PresentImageStatus"],"mod":["raii"],"struct":["Allocator","Device","Frame","FramesInFlight","Instance","Swapchain"]};
+window.SIDEBAR_ITEMS = {"enum":["AcquireImageStatus","FrameStatus","PresentImageStatus"],"mod":["raii"],"struct":["Allocator","Block","Device","Frame","FramesInFlight","Instance","OwnedBlock","Swapchain"]};
