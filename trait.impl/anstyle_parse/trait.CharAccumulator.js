d2c94a7be4356db943f9b6d72b5905be939b47aa
BradleyLyman <BradleyLyman@users.noreply.github.com>
2024-10-28 06:33:57 +0000

    deploy: feb1400b6d6284446256f5afb9e5b92879f69b65

--- trait.impl/anstyle_parse/trait.CharAccumulator.js
+++ trait.impl/anstyle_parse/trait.CharAccumulator.js
@@ -1,9 +1,3 @@
-(function() {
-    var implementors = Object.fromEntries([["anstyle_parse",[]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[20]}
+(function() {var implementors = {
+"anstyle_parse":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
