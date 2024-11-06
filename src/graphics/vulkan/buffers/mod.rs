c8f95230ad773b95d84cea0ddff3186b46b44c56
Bradley Lyman <lyman.brad3211@gmail.com>
2024-11-06 13:43:18 -0700

    move buffer implementations into submodule

---
+++ src/graphics/vulkan/buffers/mod.rs
@@ -0,0 +1,4 @@
+mod cpu_buffer;
+mod uniform_buffer;
+
+pub use self::{cpu_buffer::CPUBuffer, uniform_buffer::UniformBuffer};
--- src/graphics/vulkan/mod.rs
+++ src/graphics/vulkan/mod.rs
@@ -1,18 +1,16 @@
 mod allocator;
-mod cpu_buffer;
+mod buffers;
 mod device;
 mod frames_in_flight;
 pub mod raii;
 mod swapchain;
 mod sync_commands;
-mod uniform_buffer;
 
 pub use self::{
     allocator::{block::Block, owned_block::OwnedBlock, Allocator},
-    cpu_buffer::CPUBuffer,
+    buffers::{CPUBuffer, UniformBuffer},
     device::{Device, Instance},
     frames_in_flight::{Frame, FrameStatus, FramesInFlight},
     swapchain::{AcquireImageStatus, PresentImageStatus, Swapchain},
     sync_commands::SyncCommands,
-    uniform_buffer::UniformBuffer,
 };
