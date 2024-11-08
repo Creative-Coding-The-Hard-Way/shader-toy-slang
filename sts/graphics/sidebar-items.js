f50247828d149c9f4e18492c72c645f99b9e62b9
BradleyLyman <BradleyLyman@users.noreply.github.com>
2024-11-08 07:53:22 +0000

    deploy: 473935a9a5c1fc8497fb4ac8e286d0418e2b0243

--- sts/graphics/sidebar-items.js
+++ sts/graphics/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["ortho_projection"],"mod":["vulkan"],"struct":["FullscreenQuad","Particles","Recompiler","Texture","TextureLoader"]};
+window.SIDEBAR_ITEMS = {"fn":["ortho_projection"],"mod":["vulkan"],"struct":["FullscreenQuad","Particles","Recompiler","Sprite","SpriteLayer","StreamingSprites","Texture","TextureLoader"]};
